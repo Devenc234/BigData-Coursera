5f19fd451fcb8a4bfa178cec426370e0121d8227
Devenc234 <devendra.choudhary@zemosolabs.com>
2018-04-09 18:14:11 +0530

    course 2 is added

---
+++ Course2/big-data-2/vector/runLuceneTFIDF.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+L=/home/zemoso/Desktop/bigdata/Course2/big-data-2/lucene-5.5.0
+CLASSPATH=${CLASSPATH}:$L/core/*:$L/queryparser/*:$L/analysis/common/*:$L/demo/*
+export CLASSPATH
+
+java LuceneTFIDF "$*"
